f2f44865fe2e7ebe3af9898cd3925ad2653e498a
xRetry <marcoschwarz.at@gmail.com>
2024-05-29 17:30:50 +0200

    Add unstable pkgs

--- flake.nix
+++ flake.nix
@@ -3,13 +3,14 @@
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+        nixpkgsUnstable.url = "github:nixos/nixpkgs/nixos-unstable";
         home-manager = {
             url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";
         };
     };
 
-    outputs = { self, nixpkgs, ... }@inputs:
+    outputs = { self, nixpkgs, nixpkgsUnstable, ... }@inputs:
     let
         system = "x86_64-linux";
         pkgs = import nixpkgs {
@@ -19,16 +20,19 @@
                 "electron-25.9.0"
             ];
         };
+        pkgsUnstable = import nixpkgsUnstable {
+            inherit system;
+        };
     in {
         nixosConfigurations = {
             notebook = nixpkgs.lib.nixosSystem {
-                specialArgs = { inherit inputs system pkgs; };
+                specialArgs = { inherit inputs system pkgs pkgsUnstable; };
                 modules = [ 
                     ./notebook/configuration.nix 
                 ];
             };
             desktop = nixpkgs.lib.nixosSystem {
-                specialArgs = { inherit inputs system pkgs; };
+                specialArgs = { inherit inputs system pkgs pkgsUnstable; };
                 modules = [ 
                     ./desktop/configuration.nix 
                 ];
