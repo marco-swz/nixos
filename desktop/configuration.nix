fd33f555d28f68794cba755e944e2305fe77f323
Marco <marco.swz@hotmail.com>
2026-01-29 20:30:27 +0100

    Add steam on destop

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -18,6 +18,16 @@
         };
     };
 
+    programs = {
+        steam = {
+            enable = true;
+            remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+            dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+            localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+        };
+
+    };
+
     system.stateVersion = "23.11";
 
     services = {
